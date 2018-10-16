df6da0af534d89af058928b442ff36ebd49c08eb
HadronCollider <darcenrall@gmail.com>
2018-10-16 17:15:45 +0300

    #3 Чтение google-таблицы
    Обработка исключений при открытии таблицы, небольшое изменение структуры проекта

--- google/google_table.py
+++ google/google_table.py
@@ -1,25 +1,28 @@
-import google_instr as g_instr
+from google import google_instr as g_instr
 import configuration as conf
 import gspread.exceptions
 
 
 class GoogleTable:
 
-    Table = 0
+    Table = None
     Sheet = None
 
     def __init__(self, gc, url, sheet=0):
         """
         :param gc: gspread - GoogleAPI
         :param url: string - ссылка на таблицу
-        :param sheet: int/string - нормер/название листа таблицы
+        :param sheet: int/string - номер/название листа таблицы
         """
         try:
             self.Table = gc.open_by_url(url)
-        except gspread.exceptions:
-            print("Ошибка google_api, проверьте правильность ссылки на таблицу")
+        except gspread.exceptions.NoValidUrlKeyFound:
+            print("Не найден корректный ключ таблицы в URL, проверьте правильность ссылки на таблицу")
         else:
-            self.set_sheet(sheet)
+            try:
+                self.set_sheet(sheet)
+            except gspread.exceptions.APIError:
+                print("Ошибка google_api, проверьте правильность ссылки на таблицу")
 
     def set_sheet(self, sheet):
         """
@@ -27,8 +30,12 @@ class GoogleTable:
         :param sheet: int/string - номер/название листа таблицы
         :return: -
         """
-        sh = self.Table.get_worksheet(sheet)
-        if sh is not None:
+        sh = None
+        if type(sheet) is int:
+            sh = self.Table.get_worksheet(sheet)
+        if type(sheet) is str:
+            sh = self.Table.worksheet(sheet)
+        if sh:
             self.Sheet = sh
         else:
             print(f"Несуществующий лист таблицы: {sheet}")
@@ -39,7 +46,7 @@ class GoogleTable:
         :param num: int - номер требуемого столбца
         :return: [] - список значений всех ячеек столбца
         """
-        if self.is_sheet_none() is False:
+        if self.Sheet:
             return self.Sheet.col_values(num)
 
     def get_row(self, num):
@@ -48,7 +55,7 @@ class GoogleTable:
         :param num: int - номер требуемой строки
         :return: [] - список значений всех ячеек строки
         """
-        if self.is_sheet_none() is False:
+        if self.Sheet:
             return self.Sheet.row_values(num)
 
     def get_list(self, col, row_from, row_to):
@@ -59,15 +66,8 @@ class GoogleTable:
         :param row_to: int -конец диапазона строк
         :return: [] - список значений ячеек требуемого диапазона строк из столбца
         """
-        if self.is_sheet_none() is False:
+        if self.Sheet:
             return self.get_column(col)[row_from:row_to]
-
-    def is_sheet_none(self):
-        """
-        Проверка неудачного открытия листа таблицы
-        :return:
-        """
-        return self.Sheet is None
 
 
 if __name__ == "__main__":
