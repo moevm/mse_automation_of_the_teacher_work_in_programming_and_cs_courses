dc890b25d027103aea3198036e81857473d52648
Inoretsu <inoretsu@yandex.ru>
2018-11-04 07:05:27 +0300

    #stepic_api_two update

--- InformationsProcessor.py
+++ InformationsProcessor.py
@@ -35,33 +35,46 @@ class InformationsProcessor:
         return self.students
 
     def download_course(self):
-        id_course= self.config.get_stepic_config()['id_course']
-        name_courses=self.stepic_api.get_course_name(id_course)
-        return [id_course,name_courses]
+        """
+        Возвращает массив с перечислением id и имен курсов.
+        :return: двумерный массив с id и соответствующими именами курсов
+        """
+        id_course = self.config.get_stepic_config()['id_course']
+        name_courses =self.stepic_api.get_course_name(id_course)
+        return [id_course, name_courses]
 
     def create_jsons_course(self):
+        """
+        Создает json курсов при его отсутствии с перечислением id и имен курсов.
+        :return: json курсов
+        """
         if not self.course:
             a = self.download_course()
             list_of_cr = []
             for i in range(len(a[0])):
-                    student = {
+                    course = {
                         'id': a[0][i],
                         'name': a[1][i],
                     }
-                    list_of_cr.append(student)
-            self.course=list_of_cr
+                    list_of_cr.append(course)
+            self.course = list_of_cr
         return self.course
 
     def course_grades(self):
+        """
+        По данным из json со статистикой курсов и данным по id студентов возвращает статистику прохождения
+        курса по каждому студенту в %. При отстутствии студента на курсе возвращает "нет"
+        :return: возвращает массив с %-ми прохождения курсов по каждому студенту
+        """
         if self.students and self.course:
-            result=[]
-            grades=[self.stepic_api.get_course_statistic(c['id']) for c in self.course]
+            result = []
+            grades = [self.stepic_api.get_course_statistic(c['id']) for c in self.course]
             id_users = [st['id'] for st in self.students]
 
             for gr in grades:
-                res_dict={}
+                res_dict = {}
                 for user in id_users:
-                    res=[self.calculate_progress(i) for i in gr if str(i['user']) == user]
+                    res = [self.calculate_progress(i) for i in gr if str(i['user']) == user]
                     if res:
                         res_dict.update(res[0])
                     else:
@@ -72,8 +85,14 @@ class InformationsProcessor:
             return result
 
     def calculate_progress(self,grade_user):
-        result=grade_user['results']
-        progress=0
+        """
+        Принимает на вход статистику студента по прохождению курса, где для каждого модуля указано, пройден он или нет.
+        Возвращает процент прохождения студентом курса.
+        :param grade_user: json статистики студента по прохождению курса
+        :return: строка с % прохождения студентом курса
+        """
+        result = grade_user['results']
+        progress = 0
         for v in result.values():
             if v['is_passed']:
                 progress+=1
